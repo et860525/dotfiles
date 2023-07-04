b66ec55a0581d9e5ee95783f714d563f8bc25d41
Mango <et860525@gmail.com>
2023-07-04 20:53:21 +0800

    Update config.fish
    - Cargo path
    - pnpm path

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,13 @@ set -gx EDITOR nvim
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
+
+# set cargo path
+set PATH $HOME/.cargo/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/et860525/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
