728bea53142818719be07f7af6024167e206a5d4
Mango <et860525@gmail.com>
2023-10-05 15:35:16 +0800

    Update config.fish
    Add bun settings

--- fish/config.fish
+++ fish/config.fish
@@ -2,20 +2,19 @@
 alias docker "sudo docker"
 alias l "exa -lg --icons"
 alias ll "exa -alg --icons"
-alias vim "nvim"
+command -qv nvim && alias vim nvim
 
-set -gx EDITOR nvim
+# set default editor
+set -gx EDITOR vim
 
+set -gx TERM xterm-256color
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 
-# set cargo path
+# cargo
 set PATH $HOME/.cargo/bin $PATH
 
-# pnpm
-set -gx PNPM_HOME "/home/et860525/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
